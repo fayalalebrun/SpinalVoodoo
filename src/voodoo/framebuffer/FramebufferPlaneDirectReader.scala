425bb79abfa0af806ff9445f6ffc34dfac3226e2
Francisco Ayala Le Brun <2482295+fayalalebrun@users.noreply.github.com>
2026-04-07 17:39:19 +0200

    framebuffer: pipeline direct reader

--- src/voodoo/framebuffer/FramebufferPlaneDirectReader.scala
+++ src/voodoo/framebuffer/FramebufferPlaneDirectReader.scala
@@ -13,15 +13,13 @@ case class FramebufferPlaneDirectReader(c: Config) extends Component {
     val busy = out Bool ()
   }
 
-  val outstanding = RegInit(False)
-  val pendingLaneHi = RegInit(False)
+  val laneQueue = StreamFifo(Bool(), 8)
   val rspInput = Stream(FramebufferPlaneBuffer.ReadRsp())
-  val rspQueue = rspInput.queue(1)
-  val busyNow = outstanding || rspQueue.valid
+  val rspQueue = rspInput.queue(2)
 
-  io.readReq.ready := !busyNow && io.mem.cmd.ready
+  io.readReq.ready := io.mem.cmd.ready && laneQueue.io.push.ready
 
-  io.mem.cmd.valid := io.readReq.valid && !busyNow
+  io.mem.cmd.valid := io.readReq.valid && laneQueue.io.push.ready
   io.mem.cmd.fragment.address := alignedWordAddress(io.readReq.address)
   io.mem.cmd.fragment.opcode := Bmb.Cmd.Opcode.READ
   io.mem.cmd.fragment.length := 3
@@ -30,20 +28,16 @@ case class FramebufferPlaneDirectReader(c: Config) extends Component {
   io.mem.cmd.fragment.mask := 0
   io.mem.cmd.last := True
 
-  when(io.mem.cmd.fire) {
-    outstanding := True
-    pendingLaneHi := io.readReq.address(1)
-  }
+  laneQueue.io.push.valid := io.mem.cmd.fire
+  laneQueue.io.push.payload := io.readReq.address(1)
 
-  rspInput.valid := io.mem.rsp.valid && outstanding
-  rspInput.data := pendingLaneHi ? io.mem.rsp.fragment.data(31 downto 16) | io.mem.rsp.fragment
+  rspInput.valid := io.mem.rsp.valid && laneQueue.io.pop.valid
+  rspInput.data := laneQueue.io.pop.payload ? io.mem.rsp.fragment
+    .data(31 downto 16) | io.mem.rsp.fragment
     .data(15 downto 0)
-  io.mem.rsp.ready := rspInput.ready && outstanding
+  io.mem.rsp.ready := rspInput.ready && laneQueue.io.pop.valid
+  laneQueue.io.pop.ready := io.mem.rsp.fire && rspInput.ready
   io.readRsp << rspQueue
 
-  when(io.mem.rsp.fire) {
-    outstanding := False
-  }
-
-  io.busy := busyNow
+  io.busy := laneQueue.io.occupancy =/= 0 || rspQueue.valid
 }
